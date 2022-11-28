87c9b9e7ffdbf0c49bb800a979fa9f5e53b51980
cvbn55688 <qwas5598@gmail.com>
2022-11-28 14:03:12 +0800

    Create the attraction page and do the slideshow effect. First: In front page, user can choice the attraction. When click the attraction, it will redirect to the attraction page by JS. Second: In the attraction page, user can click the arrow to change the photo. BTW the photo will take turn by itself every three seconds.

---
+++ static/app.js
@@ -0,0 +1,173 @@
+const category_div = document.querySelector(".category");
+const serach_input = document.querySelector(".search_bar input");
+const search_button = document.querySelector(".img_box");
+const target = document.querySelector("footer");
+const callback = (entries) => {
+  if (entries[0].isIntersecting){
+    if (page != null){
+      add_attraction();
+    }else{
+      observer.unobserve(target);
+    };
+  };
+};
+const observer = new IntersectionObserver(callback, {
+threshold: 0.9,
+});
+let page = 0;
+let keyword = "";
+
+function add_attraction() {  
+  let mid_div = document.querySelector(".mid");
+  return fetch(
+    `/api/attractions?page=${page}&keyword=${keyword}`
+  )
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    attractions_data = data["data"];
+    attractions_data.forEach(data => {
+      let name = data["name"];
+      let address = data["address"];
+      let category = data["category"];
+      let description = data["description"];
+      let mrt = data["mrt"];
+      let id = data["id"];
+      let images = data["images"];
+
+      let new_card = document.createElement("div");
+      new_card.classList.add("card");
+      new_card.setAttribute("id", id);
+
+      let new_attraction = document.createElement("div");
+      new_attraction.classList.add("attraction");
+
+      let new_img = document.createElement("img");
+      new_img.src = images[0];
+
+      let attraction_name = document.createElement("p");
+      attraction_name.classList.add("attraction_name");
+      attraction_name.textContent = name;
+
+      let new_details = document.createElement("div");
+      new_details.classList.add("details");
+
+      let new_mrt = document.createElement("p");
+      new_mrt.classList.add("mrt");
+      new_mrt.textContent = mrt;
+
+      let new_CAT = document.createElement("p");
+      new_CAT.classList.add("CAT");
+      new_CAT.textContent = category;
+
+      mid_div.appendChild(new_card);
+      new_card.appendChild(new_attraction);
+      new_card.appendChild(new_details);
+      new_attraction.appendChild(new_img);
+      new_attraction.appendChild(attraction_name);
+      new_details.appendChild(new_mrt);
+      new_details.appendChild(new_CAT);
+    });
+    mid_div.addEventListener("click", function(eve){
+      if (eve.path.length == 8){
+        attraction_id = eve.path[1]["id"];
+      }else if (eve.path.length == 9){
+        attraction_id = eve.path[2]["id"];
+      }else{
+        return
+      }
+      document.location.href=`/attraction/${attraction_id}`
+    })
+    page = data["nextPage"];
+    observer.observe(target);
+    let data_length = data["data"].length;
+    return data_length;
+  });
+};
+add_attraction();
+
+search_button.addEventListener("click",attraction_search);
+async function attraction_search(){
+  let mid_div = document.querySelector(".mid");
+  let main = document.querySelector("main");
+  let search_word = document.querySelector(".search_bar input");
+  keyword = search_word["value"];
+  page = 0;
+  observer.unobserve(target);
+  mid_div.remove();
+  let new_content = document.createElement("div");
+  new_content.classList.add("mid");
+  main.appendChild(new_content);
+  data_length = await add_attraction();
+  if (data_length == 0){
+    alert("查無資料");
+  };
+};
+
+function get_category(){
+  fetch(
+    `/api/categories`
+  )
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    data = data["data"];
+    data.forEach(CAT => {
+      let CAT_div = document.createElement("div");
+      CAT_div.textContent = CAT;
+      category_div.appendChild(CAT_div);
+    });
+  });
+};
+get_category();
+
+serach_input.addEventListener("click", show_CAT);
+let CAT_box = document.querySelector(".category");
+function show_CAT(){
+  CAT_box.style.display = "grid";
+  CAT_box.addEventListener("mousedown", get_user_CAT);
+  serach_input.addEventListener("blur", close_CAT);
+};
+
+function get_user_CAT(eve){
+    CAT_name = eve.target.innerHTML.replace(/\s+/g,'');
+    if(CAT_name.substr(0,5) != "<div>"){
+      document.querySelector(".search_bar input").value = CAT_name;
+  };
+};
+
+function close_CAT(){
+  CAT_box.style.display = "None";
+};
+
+let login = document.querySelector(".login")
+login.addEventListener("click", show_black_screen)
+let close_button = document.querySelector(".member_login img")
+close_button.addEventListener("click", close_black_screen)
+let login_page = document.querySelector(".full_screen")
+let login_div = document.querySelector(".member_login")
+let sign_div = document.querySelector(".signup")
+function show_black_screen(){
+  
+  login_page.style.display = "flex"
+}
+function close_black_screen(){
+  login_page.style.display = "None"
+}
+
+let signup_button = document.querySelector(".signup_button");
+signup_button.addEventListener("click", show_signup)
+function show_signup(){
+  login_div.style.display = "None"
+  sign_div.style.display = "flex"
+}
+
+let login_button = document.querySelector(".login_button");
+login_button.addEventListener("click", show_login)
+function show_login(){
+  login_div.style.display = "flex"
+  sign_div.style.display = "None"
+}
+
---
+++ static/attraction.js
@@ -0,0 +1,189 @@
+let url = location.href;
+let arrtacrion_id = url.split("/").pop();
+
+function get_attraction() {  
+    return fetch(
+      `/api/attraction/${arrtacrion_id}`
+    )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        data = data["data"];
+        attraction_name = data["name"];
+        category = data["category"].replace(/\s+/g,'');
+        mrt = data["mrt"];
+        description = data["description"];
+        address = data["address"];
+        transport = data["transport"];
+        imgs = data["images"];
+
+        const img_div = document.querySelector(".imges")
+        imgs.forEach(img => {
+          let new_img = document.createElement("img");
+          new_img.src = img;
+          img_div.appendChild(new_img);
+
+        });
+
+        const arrtaction_title_div = document.querySelector(".arrtaction_title");
+        let attraction_title = document.createElement("p");
+        attraction_title.classList.add("profile_title");
+        attraction_title.textContent = attraction_name;
+        arrtaction_title_div .appendChild(attraction_title);
+
+        let attraction_subtitle = document.createElement("p");
+        attraction_subtitle.classList.add("profile_subtitle");
+        if (mrt){
+            attraction_subtitle.textContent = `${category} at ${mrt}`
+        }else{
+            attraction_subtitle.textContent = `${category}`
+        }
+        arrtaction_title_div .appendChild(attraction_subtitle)
+
+        const description_div = document.querySelector(".description");
+        let description_p = document.createElement("p");
+        description_p.textContent = description;
+        description_div.appendChild(description_p)
+
+        const address_div = document.querySelector(".address");
+        let address_p = document.createElement("p");
+        address_p.textContent = address;
+        address_div.appendChild(address_p);
+
+        const transport_div = document.querySelector(".transport");
+        let transport_p = document.createElement("p");
+        transport_p.textContent = transport;
+        transport_div.appendChild(transport_p);
+        
+        img_items = document.querySelectorAll(".imges img")
+        return img_items
+    });
+};
+let imges = get_attraction();
+let current = 0;
+
+imges.then((value) => {
+    const picture_div = document.querySelector(".dot")
+    for (let i = 0; i < value.length; i++){
+      let new_dot = document.createElement("div");
+      new_dot.classList.add("dot_child");
+      new_dot.setAttribute("id", i);
+      picture_div.appendChild(new_dot)
+    };
+    let dot_div = document.querySelectorAll(".dot_child");
+    dot_div[current].style.backgroundColor = "#000000";  
+    // let dot_parent_div = document.querySelector(".dot");
+    picture_div.addEventListener("click",get_user_dot);
+    function get_user_dot(eve){
+      id = eve.target.id;
+      if (id != ""){
+        dot_div[current].style.backgroundColor = "#ffffff"; 
+        change_pic(Number(id))
+      }
+    }
+    picture_div.addEventListener("mouseover",(eve) => {
+      id = eve.target.id;
+      if (id != current && id != ""){
+        eve.target.style.backgroundColor = "#000000"; 
+        eve.target.style.animation = "appear_dot 0.25s forwards"
+      }
+    })
+    picture_div.addEventListener("mouseout",(eve) => {
+      id = eve.target.id;
+      if (id != current && id != ""){
+        eve.target.style.backgroundColor = "#ffffff"; 
+        eve.target.style.animation = "disappear_dot 0.25s forwards"
+      }
+    })
+    function change_pic(id){
+      if (id > current){
+        value[current].style.animation = "hideRight 1s forwards";
+        current = id;
+        value[current].style.animation =
+        "showRight 1s forwards";
+      }
+      if (id < current){
+        value[current].style.animation = "hideLeft 1s forwards";
+        current = id;
+        value[current].style.animation =
+        "showLeft 1s forwards";
+      }
+      dot_div[current].style.backgroundColor = "#000000";  
+    }
+    function next_pic(){
+      value[current].style.animation = "hideRight 1s forwards";
+      value[current >= value.length - 1 ? 0 : current + 1].style.animation =
+      "showRight 1s forwards";
+      dot_div[current].style.backgroundColor = "#ffffff"; 
+      if (current < value.length - 1){
+        current++;
+        dot_div[current].style.backgroundColor = "#000000";  
+      }else{
+        current = 0;
+        dot_div[current].style.backgroundColor = "#000000"; 
+      };
+    };
+    function precious_pic(){
+      value[current].style.animation = "hideLeft  1s forwards";
+      value[current > 0 ? current - 1 : value.length -1].style.animation =
+      "showLeft 1s forwards";
+      dot_div[current].style.backgroundColor = "#ffffff"; 
+      if (current > 0 ){
+        current--;
+        dot_div[current].style.backgroundColor = "#000000";  
+      }else{
+        current = value.length -1;
+        dot_div[current].style.backgroundColor = "#000000";  
+      };
+    };
+    let next_button = document.querySelector(".right_arrow");
+    let previous_button = document.querySelector(".left_arrow");
+    let all_pic_touch = document.querySelector(".picture");
+    next_button.addEventListener("click", next_pic);
+    previous_button.addEventListener("click", precious_pic);
+    let pic_turn = window.setInterval(next_pic, 3000);
+
+  });
+
+
+  
+let login = document.querySelector(".login")
+login.addEventListener("click", show_black_screen)
+let close_button = document.querySelector(".member_login img")
+close_button.addEventListener("click", close_black_screen)
+let login_page = document.querySelector(".full_screen")
+let login_div = document.querySelector(".member_login")
+let sign_div = document.querySelector(".signup")
+
+function show_black_screen(){
+  login_page.style.display = "flex"
+}
+function close_black_screen(){
+  login_page.style.display = "None"
+}
+
+let signup_button = document.querySelector(".signup_button");
+signup_button.addEventListener("click", show_signup)
+function show_signup(){
+  login_div.style.display = "None"
+  sign_div.style.display = "flex"
+}
+
+let login_button = document.querySelector(".login_button");
+login_button.addEventListener("click", show_login)
+function show_login(){
+  login_div.style.display = "flex"
+  sign_div.style.display = "None"
+}
+
+const morning = document.querySelector(".morning");
+const afternoon = document.querySelector(".afternoon");
+const cost = document.querySelector("#p_6");
+morning.addEventListener("click", () => {
+  cost.textContent = "新台幣2000元"
+});
+afternoon.addEventListener("click", () => {
+  cost.textContent = "新台幣2500元"
+})
+
